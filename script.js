acf617a4dda86e44dd11fa46ca6846340b224c68
shree-datta-krupa-audumbar <pussigobbler@gmail.com>
2025-01-06 23:52:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,98 @@ validCredentials = Array.from({ length: 29 }, (_, i) => ({
 }));
 
 // Add admin credentials
-validCredentials.push({
-    email: 'admin@example.com',
-    password: '1234'
-},
-   { email: 'yy@h',
-    password: '11'      });
+validCredentials.push(
+    { email: 'admin@example.com',
+    password: '1234'},
+
+   { email: 'abhijitd2212@gmail.com',
+    password: '05-09-1961' },
+
+    { email: 'rajendrafaria@gmail.com',
+    password: '01-11-1967' },
+
+    { email: 'soni.pravin788@gmail.com',
+    password: '13-07-1961' },
+
+    { email: 'vrpoladia@gmail.com',
+    password: '26-10-1953' },
+
+    { email: 'surendravdeshpande@gmail.com',
+    password: '12-07-1935' },
+
+    { email: 'prabhakarrahalkar@gmail.com',
+    password: '14-07-1939' },
+
+    { email: 'deepakmokashi@yahoo.com',
+    password: '28-02-1961' },
+
+    { email: 'jatin.faria@gmail.com',
+    password: '30-01-1975' },
+
+    { email: 'ravisatra90@gmail.com',
+    password: '01-03-1990' },
+
+    { email: 'rajatsatra@yahoo.com',
+    password: '15-07-1967' },
+
+    { email: 'meenakavitake@gmail.com',
+    password: '22-04-1963' },
+
+    { email: 'mayuris21@gmail.com',
+    password: '21-10-1980' },
+
+    { email: 'sunilplokhande9999@gmail.com',
+    password: '11-05-1969' },
+
+    { email: 'manasdixit07@gmail.com',
+    password: '29-05-1993' },
+
+    { email: 'ammahajan7@gmail.com',
+    password: '04-11-1965' },
+
+    { email: 'anika.bhat7@gmail.com',
+    password: '02-06-1943' },
+
+    { email: 'kaushalpujara89@gmail.com',
+    password: '' },
+
+    { email: 'pritambhogate@gmail.com',
+    password: '22-07-1976' },
+
+    { email: 'riswadkar.harshad@gmail.com',
+    password: '26-06-1979' },
+
+    { email: 'patwardhan.yogesh@gmail.com',
+    password: '09-11-1952' },
+
+    { email: 'neharkhandekar@gmail.com',
+    password: '25-04-1960' },
+
+    { email: 'papuku@yahoo.com',
+    password: '04-05-1970' },
+
+    { email: 'koumudie@yahoo.com',
+    password: '21-05-1975' },
+
+    { email: 'seema_dighe@yahoo.com',
+    password: '19-02-1968' },
+
+    { email: 'deepa021955@gmail.com',
+    password: '01-02-1955' },
+
+    { email: 'ravisatra90@gmail.com',
+    password: '02-05-1963' },
+
+    { email: 'ravisatra90@gmail.com',
+    password: '' },
+
+    { email: 'lp.leenapatil@gmail.com',
+    password: '05-02-1936' },
+
+    { email: 'lp.leenapatil@gmail.com',
+    password: '05-02-1936' }
+);
+    
 
 // Check if user is logged in
 function checkAuth() {
